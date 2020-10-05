d0cfb85d94c79d9838de923cc4c5e25949f7bb40
Luis <luyimiranda07@gmail.com>
2020-10-04 21:18:39 -0500

    Updated discard repeated algo to use sets to be more efficient

--- scraper/builds_links_scraper.py
+++ scraper/builds_links_scraper.py
@@ -3,7 +3,7 @@ import time
 import json
 import utilities
 import traceback
-from random import randint
+from random import choice, randint
 from bs4 import BeautifulSoup
 
 def get_links():
@@ -18,20 +18,25 @@ def get_links():
 
 def scrape_links():
     num_pages = 1
-    user_agent = utilities.get_user_agent()
+    user_agents = utilities.get_user_agent()
+    proxies = utilities.get_proxies()
     file_name = 'builds.json'
-    links = []
+    links_format = lambda links: [{'link': link} for link in links]
+    links_scraped = []
     for i in range(1, num_pages+1):
         try:
+            proxy = choice(proxies)
+            user_agent = choice(user_agents)
             url = utilities.parse_url(page=i)
-            browser = utilities.get_driver(user_agent=user_agent)
+            browser = utilities.get_driver(user_agent, proxy)
             browser.get(url)
             time.sleep(randint(2, 5))
             soup = BeautifulSoup(browser.page_source, 'html.parser')
 
-            # Get the link of all build cards in a single page
+            # Get the link of all build cards per page
             links_el = soup.find_all("a", {"class": "logGroup__target"}, href=True)
-            links = [{'link': a['href']} for a in links_el]
+            links_el = [l_el['href'] for l_el in links_el]
+            links_scraped += links_format(links_el)
             browser.close()
         except Exception as e:
             browser.close()
@@ -41,24 +46,21 @@ def scrape_links():
     if not os.path.exists(f'./{file_name}'):
         # Save build href in a json file
         with open(f'{file_name}', 'w') as f:
-            json.dump(links, f, indent=2)
+            json.dump(links_scraped, f, indent=2)
 
     else:
         # Only save the new links
         with open(f'{file_name}', 'r+') as f:
-            new_links = []
-            data = json.load(f)
-            data_list = [d['link'] for d in data]
-            for link in links:
-                if link['link'] not in data_list:
-                    new_links.append(link)
-                else:
-                    break
-            # Update json with new links
-            if new_links:
+            old_links = json.load(f)
+            old_links = [l['link'] for l in old_links]
+            #  Get the difference between scraped links and scraped old links
+            links_diff = set([l['link'] for l in links_scraped])
+            links_diff.difference_update(old_links)
+
+            if links_diff:
                 f.seek(0)
-                new_links.extend(data)
-                del data
+                new_links = links_format([*links_diff, *old_links])
+                del old_links
                 json.dump(new_links, f, indent=2)
 
 if __name__ == '__main__':
